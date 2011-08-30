00d97efcf1075cbbb30db32c42bbd24d092f21bf
Brandon Turner <bturner@bltweb.net>
2011-08-30 14:43:58 -0500

    Added the mapleta_parent_id field

--- lib/content_repository_client/product.rb
+++ lib/content_repository_client/product.rb
@@ -1,6 +1,6 @@
 module Thinkwell::ContentRepository
   class Product
     include HashInitialize
-    attr_accessor :id, :name, :default_course_id
+    attr_accessor :id, :name, :default_course_id, :mapleta_parent_id
   end
 end
