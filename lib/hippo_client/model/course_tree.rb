4b8402bbb1e0284d9e69a97f5c5caab85972683b
Brandon Turner <bt@brandonturner.net>
2011-11-17 16:43:53 -0600

    Allow easily iterating over the nodes in a CourseTree

--- lib/hippo_client/model/course_tree.rb
+++ lib/hippo_client/model/course_tree.rb
@@ -2,6 +2,7 @@ module Thinkwell::Hippo
   module Model
     class CourseTree
       include HashInitialize
+      include Enumerable
       attr_accessor :id, :child_nodes, :created_at, :updated_at
 
       def child_nodes=(value)
@@ -9,6 +10,25 @@ module Thinkwell::Hippo
           v.is_a?(Hash) ? Node.new(v) : v
         end if value.is_a?(Array)
         @child_nodes = value
+      end
+
+
+      # Iterates over all nodes in depth-first order
+      def each
+        each_with_depth {|node, depth| yield node }
+      end
+
+      # Iterates over all nodes in depth-first order
+      # including the depth of each node (starting at depth=0)
+      def each_with_depth
+        fn = lambda do |child_nodes, depth|
+          child_nodes.each do |node|
+            yield node, depth
+            fn.call(node.child_nodes, depth+1) if node.child_nodes
+          end
+        end
+
+        fn.call(self.child_nodes, 0)
       end
 
     end
---
+++ spec/hippo_client/model/course_tree_spec.rb
@@ -0,0 +1,56 @@
+require 'spec_helper'
+
+module Thinkwell::Hippo::Model
+describe CourseTree do
+
+  before(:each) do
+    @tree = CourseTree.new({
+      :id => '1',
+      :child_nodes => [
+        {:id => '1', :name => '1', :number => '1', :child_nodes => [
+          {:id => '2', :name => '1.1', :number => '1.1', :child_nodes => [
+            {:id => '3', :name => '1.1.1', :number => '1.1.1'},
+          ]}
+        ]},
+        {:id => '4', :name => '2', :number => '2', :child_nodes => [
+          {:id => '5', :name => '2.1', :number => '2.1'},
+          {:id => '6', :name => '2.7', :number => '2.7'},
+        ]},
+      ]
+    })
+  end
+
+
+  context "#each" do
+    it "iterates through the tree using depth-first ordering" do
+      ary = []
+      @tree.each {|node| ary << node.id.to_i}
+      ary.should == [1,2,3,4,5,6]
+    end
+
+    it "supports breaking the iterator" do
+      ary = []
+      @tree.each do |node|
+        ary << node.id.to_i
+        break if node.id == '3'
+      end
+      ary.should == [1,2,3]
+    end
+  end
+
+  context "#each_with_depth" do
+    it "includes the depth of each node" do
+      ary = []
+      @tree.each_with_depth {|node, depth| ary << depth}
+      ary.should == [0, 1, 2, 0, 1, 1]
+    end
+  end
+
+  context "#map" do
+    it "iterates over the tree in depth-first order" do
+      @tree.map {|node| node.id.to_i}.should == [1,2,3,4,5,6]
+    end
+  end
+
+end
+end
