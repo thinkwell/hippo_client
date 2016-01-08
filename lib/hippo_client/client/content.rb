66a95638fe7717c23375174a80a7802979beb4ca
Palo Chalupka <paloc@thinkwell.com>
2016-01-08 09:52:17 -0600

    Let content be accessed without course context. Course context doesn't currently add anything and prevents shared content.

--- lib/hippo_client/client/content.rb
+++ lib/hippo_client/client/content.rb
@@ -6,7 +6,8 @@ module Thinkwell::Hippo
       # Possible options:
       #   version
       def content(courseId, contentId, opts={})
-        path = courseId.nil? ? "content/#{contentId}" : "courses/#{courseId}/content/#{contentId}"
+        #path = courseId.nil? ? "content/#{contentId}" : "courses/#{courseId}/content/#{contentId}"
+        path = "content/#{contentId}"
         hydrate :Content, fetch(path, opts)
       end
 
@@ -15,7 +16,8 @@ module Thinkwell::Hippo
       #   template
       #   parameters
       def contentView(courseId, contentId, format='html', opts={})
-        coursePath = (courseId ? "courses/#{courseId}/" : "")
+        #coursePath = (courseId ? "courses/#{courseId}/" : "")
+        coursePath = ''
         fetch("#{coursePath}content/#{contentId}/view", opts, format)
       end
 
